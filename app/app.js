b712e56e01fda927d88a5fc4beda014c46b59e54
Maria Thomas <mariathomas310212@gmail.com>
2023-03-25 00:51:34 +0000

    fetch data

--- app/app.js
+++ app/app.js
@@ -10,17 +10,51 @@ app.use(express.static("static"));
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
+// Use the Pug templating engine
+app.set('view engine', 'pug');
+app.set('views', './app/views');
+
+// Get the models
+const { User } = require("./models/user");
+
 // Create a route for root - /
 app.get("/", function(req, res) {
-    res.send("Hello world!");
+    // Set up an array of data
+    // Send the array through to the template as a variable called data
+    res.render("index");
 });
+
+app.get("/RSVP", function(req, res) {
+    res.render("RSVP");
+});
+
+// Task 2 display a formatted list of students
+app.get("/admin", function(req, res) {
+    var sql = 'select * from UserProfile';
+    db.query(sql).then(results => {
+    	    // The rows will be in a variable called data
+        res.render('all_users', {data: results});
+    });
+});
+
+// Single student page, using MVC pattern
+app.get("/single_user/:id", async function (req, res) {
+    var stId = req.params.id;
+    var user = new User(stId);
+    await user.getUserDetails();
+    await user.getCakeName();
+    await user.getVenuesName();
+    await user.getDecorationName();
+    await user.getFlowerName();
+    res.render('user', {user:user});
+});
+
 
 // Create a route for testing the db
 app.get("/db_test", function(req, res) {
     // Assumes a table called test_table exists in your database
     sql = 'select * from test_table';
     db.query(sql).then(results => {
-        console.log(results);
         res.send(results)
     });
 });
